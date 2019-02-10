cd9c8513c7d3685f3f3753a7ef094c3f362397ac
Manmeet <manmeetrana06@gmail.com>
2019-02-10 15:07:56 +0530

    code added for update sponsor

--- routes/club.js
+++ routes/club.js
@@ -76,18 +76,61 @@ router.post('/register',function(req,res){
     });
 
 router.get('/login',function(req,res){
-	res.render('login');
+  res.render('login');
 });
-
+/*--------------------------SHOW SPONSOR FOR ADMIN AND UPDATE BUTTON------------------------*/
 router.get('/showsponsor',BrixxloggedIn,function(req,res){
     Sponsor.find({ },function(err,result){
       if(err) 
         console.log("Some Error Occur");
       else
-        res.json(result);
+        res.render('adminshowsponsor',{sponsor : result});
     });
 });  
 
+/*---------------FORM FOR GETTING ID OF SPONSOR AND RENDERING THE UPDATE FORM---------------*/
+
+router.post('/updateFormSponsor',BrixxloggedIn,function(req,res){
+  Sponsor.findOne({_id:req.body.id},function(error,result){
+    if(error)
+    {
+      console.log("Error Occured");
+    }
+    else
+    {
+      res.render('updatesponsor',{sponsor : result});
+    }
+  })
+})
+
+/*----------------------UPDATE SPONSOR-----------------------------------------------------*/
+router.post('/updateSponsor',BrixxloggedIn,function(req,res){
+  var query = req.body.id;
+  var name = req.body.name;
+  var title = req.body.title;
+  var rank = req.body.rank;
+  var logo = req.body.logo;
+  var website = req.body.website;
+  var newValues = {$set : {
+    name: name,
+    title: title,
+    rank: rank,
+    logo: logo,
+    website: website
+  }};
+  Sponsor.updateOne({_id : query},newValues,function(error,result){
+    if(error)
+    {
+      console.log("Error Occured");
+    }
+    else
+    {
+      res.redirect('/club/showsponsor');
+    }
+  })
+})
+
+/*--------------------POST ROUTE FOR ADD SPONSOR--------------------------------------*/
 router.post('/addsponsor', BrixxloggedIn, function(req, res, next) {
         var name = req.body.name;
         var title = req.body.title;
@@ -122,8 +165,10 @@ router.get('/addsponsor', BrixxloggedIn, function(req, res, next) {
 });
 
 router.get('/addevent', require('connect-ensure-login').ensureLoggedIn('/club/login'), function(req,res){
-	res.render('addevent');
+  res.render('addevent');
 })
+
+/*------------------POST ROUTE FOR ADD EVENT-------------------------------------------*/
 
 router.post('/addevent',require('connect-ensure-login').ensureLoggedIn('/club/login'),function(req,res){
   var clubname = req.user.username;
@@ -176,6 +221,73 @@ router.post('/addevent',require('connect-ensure-login').ensureLoggedIn('/club/lo
 });
 
 
+/*------------------FORM RENDERING FOR THE UPDATE EVENT------------------------------------*/
+router.post('/updateFormEvent',require('connect-ensure-login').ensureLoggedIn('/club/login'),function(req,res){
+  Event.findOne({_id : req.body.id},function(error,result){
+      if(error)
+      {
+        console.log("Error Occured");
+      }
+      else
+      {
+        res.render('updateevent',{event : result});
+      }
+  })
+})
+
+/*--------------------------ROUTE FOR UPDATING THE EVENT---------------------------------*/
+router.post('/updateevent',require('connect-ensure-login').ensureLoggedIn('/club/login'),function(req,res){
+  var query = req.body.id;
+  var clubname = req.user.username;
+  var title = req.body.title;
+  var category = req.body.category;
+  var eventtype = req.body.eventtype;
+  var coordinatorName = req.body.coordinatorName;
+  var coordinatorPhone = req.body.coordinatorPhone;
+  var prize1 = req.body.prize1;
+  var prize2 = req.body.prize2;
+  var prize3 = req.body.prize3;
+  var description = req.body.description;
+  var photolink = req.body.photolink;
+  var venue = req.body.venue;
+  var fee = req.body.fee;
+  var date = req.body.date;
+  var starttime = req.body.starttime;
+  var endtime = req.body.endtime;
+  var newValues = {$set : {
+    title : title,
+    eventtype : eventtype,
+    category : category,
+    description : description,
+    venue : venue,
+    photolink : photolink,
+    prizes : {
+      prize1 : prize1,
+      prize2 : prize2,
+      prize3 : prize3,
+    },
+    coordinator : [{name : coordinatorName,phone : coordinatorPhone}],
+    fees : fee,
+    date : date,
+    starttime : starttime,
+    endtime : endtime,
+  }};
+  Event.updateOne({_id:query},newValues,function(error,result){
+    if(error)
+    {
+      console.log("Error Occcured");
+    }
+    else
+    {
+      console.log(result);
+      res.redirect('/club/showevent');
+    }
+  })
+
+})
+
+
+
 /*route for showing list of events for the club */
 router.get('/showevent',require('connect-ensure-login').ensureLoggedIn('/club/login'),function(req,res){
     Event.find({clubname:req.user.username},function(err,result){
@@ -185,10 +297,12 @@ router.get('/showevent',require('connect-ensure-login').ensureLoggedIn('/club/lo
       }
       else
       {
-        res.json(result);
+        res.render('adminshowevent',{event : result});
       }
     })
 });
+
+/*-------------------------------RENDERING FORM FOR DELETING THE EVENT----------------------*/
 router.get('/deleteEvent',require('connect-ensure-login').ensureLoggedIn('/club/login'),function(req,res){
   Event.find({clubname:req.user.username},function(err,result){
       if(err)
@@ -201,6 +315,8 @@ router.get('/deleteEvent',require('connect-ensure-login').ensureLoggedIn('/club/
       }
     })
 });
+
+/*--------------------------POST ROUTE FOR DELETE EVENT-------------------------------*/
 
 router.post('/deleteevent',require('connect-ensure-login').ensureLoggedIn('/club/login'),function(req,res){
   Event.deleteOne({_id : req.body.id},function(err,result){
@@ -216,7 +332,7 @@ router.post('/deleteevent',require('connect-ensure-login').ensureLoggedIn('/club
   })
 });
 
-/*Delete Sponsor */
+/*--------------------------GET ROUTE FOR DELETE SPONSOR--------------------- */
 
 router.get('/deletesponsor',BrixxloggedIn,function(req,res){
   Sponsor.find({},function(err,sponsor){
@@ -231,6 +347,7 @@ router.get('/deletesponsor',BrixxloggedIn,function(req,res){
   })
 });
 
+/*---------------------------POST ROUTE FOR DELETING SPONSOR----------------------*/
 router.post('/deletesponsor',BrixxloggedIn,function(req,res){
   Sponsor.deleteOne({_id : req.body.id},function(err,result){
     if(err)
