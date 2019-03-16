4c1366312b736113105f5bd35a08b02c9d4dfab3
Manmeet <manmeetrana06@gmail.com>
2019-03-17 00:38:57 +0530

    added jwt for login and made some changes in api

--- routes/club.js
+++ routes/club.js
@@ -1,85 +1,75 @@
 var express = require('express');
 let router = express.Router();
-var passport = require('passport');
-const session = require('express-session');
-var LocalStrategy = require('passport-local').Strategy;
 var Admin = require('../models/admins');
 var Sponsor =  require('../models/sponsors');
 var Event =  require('../models/events');
+var jwt = require('jsonwebtoken'); 
+var config = require('../config'); 
 
 function BrixxloggedIn(req, res, next) {
-  console.log(req.user.username);
-    if (req.user.username == 'brixx') {
-        next();
-    } else {
-        res.redirect('/');
-    }
+  Admin.findById(req.userId, function (err, user) {
+    if (err) return res.status(500).send('Error on the server.');
+    if (!user) return res.status(404).send('No user found.');
+    if(user.username==='brixx')
+     next();
+    else
+      return res.status(500).send({ auth: false, message: 'Failed to authenticate token for brixx.' });
+  });
 }
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    Admin.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (user.password != password) { return done(null, false); }
-      return done(null, user);
-    });
-  }
-));
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-
- router.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
+function verifyToken(req, res, next) {
+  var token = req.headers['x-access-token'];
+  if (!token) 
+    return res.status(403).send({ auth: false, message: 'No token provided.' });
+  jwt.verify(token, config.secret, function(err, decoded) {      
+    if (err) 
+      return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });    
+    req.userId = decoded.id;
+    next();
   });
 
- router.get('/',require('connect-ensure-login').ensureLoggedIn('/club/login'), function(req,res){
-  res.render('index');
+}
+
+router.post('/login', function(req, res) {
+
+  Admin.findOne({ username: req.body.username }, function (err, user) {
+    if (err) return res.status(500).send('Error on the server.');
+    if (!user) return res.status(404).send('No user found.');
+    var passwordIsValid = (req.body.password===user.password);
+    if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+    var token = jwt.sign({ id: user._id }, config.secret, {
+      expiresIn: 86400 
+    });
+    res.status(200).send({ auth: true, token: token });
+  });
+
 });
 
-  router.get('/logout',
-        function (req, res) {
-            req.logout();
-            res.redirect('/');
-        });
 
-router.get('/register',function(req,res){
-      res.render('club_register');
+router.get('/logout', function(req, res) {
+  res.status(200).send({ auth: false, token: null });
 });
 
-router.post('/register',function(req,res){
-        var username = req.body.username;
-        var password = req.body.password;
-        var newAdmin = new Admin({
-            username: username,
-            password: password
-        });
+router.post('/register',verifyToken,BrixxloggedIn, function(req, res) {
 
-        Admin.create(newAdmin,function(err,admin){
-            if(err)
-            {
-                res.redirect('/register');
-            }
-            else
-            {
-                res.redirect('/');
-            }
-        })
+  Admin.create({
+    username : req.body.username,
+    password : req.body.password,
+  }, 
+  function (err, user) {
+    if (err) return res.status(500).send("There was a problem registering the user.");
+
+    var token = jwt.sign({ id: user._id }, config.secret, {
+      expiresIn: 86400 
     });
 
-router.get('/login',function(req,res){
-  res.render('login');
+    res.status(200).send({ auth: true, token: token });
+  });
+
 });
+
 /*--------------------------SHOW SPONSOR FOR ADMIN AND UPDATE BUTTON------------------------*/
-router.get('/showsponsor',BrixxloggedIn,function(req,res){
+router.get('/showsponsor',verifyToken,BrixxloggedIn,function(req,res){
     Sponsor.find({ },function(err,result){
       if(err) 
         console.log("Some Error Occur");
@@ -164,13 +154,13 @@ router.get('/addsponsor', BrixxloggedIn, function(req, res, next) {
     res.render('addsponsor')
 });
 
-router.get('/addevent', require('connect-ensure-login').ensureLoggedIn('/club/login'), function(req,res){
+router.get('/addevent', verifyToken, function(req,res){
   res.render('addevent');
 })
 
 /*------------------POST ROUTE FOR ADD EVENT-------------------------------------------*/
 
-router.post('/addevent',require('connect-ensure-login').ensureLoggedIn('/club/login'),function(req,res){
+router.post('/addevent',verifyToken,function(req,res){
   var newEvent  = new Event({
     clubname : req.user.username,
     hitCount : 0,
@@ -208,7 +198,7 @@ router.post('/addevent',require('connect-ensure-login').ensureLoggedIn('/club/lo
 
 
 /*------------------FORM RENDERING FOR THE UPDATE EVENT------------------------------------*/
-router.post('/updateFormEvent',require('connect-ensure-login').ensureLoggedIn('/club/login'),function(req,res){
+router.post('/updateFormEvent',verifyToken,function(req,res){
   Event.findOne({_id : req.body.id},function(error,result){
       if(error)
       {
@@ -222,7 +212,7 @@ router.post('/updateFormEvent',require('connect-ensure-login').ensureLoggedIn('/
 })
 
 /*--------------------------ROUTE FOR UPDATING THE EVENT---------------------------------*/
-router.post('/updateevent',require('connect-ensure-login').ensureLoggedIn('/club/login'),function(req,res){
+router.post('/updateevent',verifyToken,function(req,res){
   var query = req.body.id;
   var newValues = {$set : {
     title : req.body.title,
@@ -260,7 +250,7 @@ router.post('/updateevent',require('connect-ensure-login').ensureLoggedIn('/club
 
 
 /*route for showing list of events for the club */
-router.get('/showevent',require('connect-ensure-login').ensureLoggedIn('/club/login'),function(req,res){
+router.get('/showevent',verifyToken,function(req,res){
     Event.find({clubname:req.user.username},function(err,result){
       if(err)
       {
@@ -274,7 +264,7 @@ router.get('/showevent',require('connect-ensure-login').ensureLoggedIn('/club/lo
 });
 
 /*-------------------------------RENDERING FORM FOR DELETING THE EVENT----------------------*/
-router.get('/deleteEvent',require('connect-ensure-login').ensureLoggedIn('/club/login'),function(req,res){
+router.get('/deleteEvent',verifyToken,function(req,res){
   Event.find({clubname:req.user.username},function(err,result){
       if(err)
       {
@@ -289,7 +279,7 @@ router.get('/deleteEvent',require('connect-ensure-login').ensureLoggedIn('/club/
 
 /*--------------------------POST ROUTE FOR DELETE EVENT-------------------------------*/
 
-router.post('/deleteevent',require('connect-ensure-login').ensureLoggedIn('/club/login'),function(req,res){
+router.post('/deleteevent',verifyToken,function(req,res){
   Event.deleteOne({_id : req.body.id},function(err,result){
     //console.log(result);
     if(err)
